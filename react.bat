a113ec49dcca57b47b90c1d5c1d156e3fb202f6d
Vincent <vincent.seube@free.fr>
2023-06-12 18:45:36 +0200

    Project Init

---
+++ react.bat
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Script a la racine d'un projet React
+echo "---- Installation Sass  et creation de l'architecture Front ----"
+npm install sass & cd src & mkdir assets & mkdir components & mkdir datas & mkdir pages & mkdir styles & echo "Installation terminee"
+exit
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div className="Home">fraf</div>
+}
+
+export default Home
